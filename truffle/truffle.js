555607b411d2765fa44be1c83ea173ace1ce9374
Abhishek Warokar <abhishek11@live.in>
2017-12-08 14:10:36 +0530

    :hammer: truffle / infura

--- truffle/truffle.js
+++ truffle/truffle.js
@@ -1,9 +1,17 @@
+var HDWalletProvider = require('truffle-hdwallet-provider');
+
+var mnemonic = 'come eye busy museum magnet long venue cash trend kitchen voice ugly';
+
 module.exports = {
   networks: {
     development: {
       host: 'localhost',
       port: 8545,
       network_id: '*' // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/qC8vHEIJuuyDTTCKXf2m'),
+      network_id: 3
     }
   }
 };
